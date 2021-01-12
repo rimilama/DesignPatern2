e6b5e727f2134edba9283aa87b18dc42b2b4b087
Rimilya <a.daeffler@ludus-academie.com>
2021-01-12 12:08:39 +0100

    Command pattern

---
+++ Assets/Command/Script/Command.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    protected float Move = 1f;
+    public abstract void Execute(Transform Box);
+}
+
+public class Avancer : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(Box.forward * Move);
+    }
+}
+
+public class Reculer : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(-Box.forward * Move);
+    }
+}
+
+public class Droite : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(Box.right * Move);
+    }
+}
+
+public class Gauche : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(-Box.right * Move);
+    }
+}
+
+public class Monter : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(Box.up * Move);
+    }
+}
+
+public class Descndre : Command
+{
+    public override void Execute(Transform Box)
+    {
+        Box.Translate(-Box.up * Move);
+    }
+}
---
+++ Assets/Command/Script/GameControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    public Transform BoxTransform;
+    private Command ButZ, ButQ, ButS, ButD, ButA, ButE;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButZ = new Avancer();
+        ButQ = new Gauche();
+        ButS = new Reculer();
+        ButD = new Droite();
+        ButA = new Monter();
+        ButE = new Descndre();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ButZ.Execute(BoxTransform);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ButQ.Execute(BoxTransform);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            ButS.Execute(BoxTransform);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ButD.Execute(BoxTransform);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            ButA.Execute(BoxTransform);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            ButE.Execute(BoxTransform);
+        }
+    }
+}
